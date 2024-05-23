78027329534a8199750ae7477ff2edc79a93cdc0
sayemhossin <sayemhossin22@gmail.com>
2024-05-24 03:15:07 +0600

    6th day

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function run() {
 
     // middlewares
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers.authorization)
+      // console.log('inside verify token', req.headers.authorization)
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'unauthorized access' })
       }
@@ -68,7 +68,7 @@ async function run() {
       const query = { email: email }
       const user = await userCollection.findOne(query)
       const isAdmin = user?.role === 'admin'
-      if(!isAdmin){
+      if (!isAdmin) {
         return res.status(403).send({ message: 'forbidden access' })
       }
       next()
@@ -80,7 +80,7 @@ async function run() {
 
     //  users related api
 
-    app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
@@ -101,7 +101,7 @@ async function run() {
 
 
     //  set role  admin {from (AllUsers) client site}
-    app.patch('/users/admin/:id',verifyToken,verifyAdmin, async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -130,7 +130,7 @@ async function run() {
     })
 
 
-    app.delete('/users/:id',verifyToken,verifyAdmin, async (req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query)
@@ -148,10 +148,62 @@ async function run() {
       const result = await menuCollection.find().toArray()
       res.send(result)
     })
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray()
       res.send(result)
     })
+
+
+
+
+
+
+
+
+
 
 
     //  carts collections
